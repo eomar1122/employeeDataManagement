ce10d084f3db372ab6004054fd79278b7c33dc5f
Michael Kovacic <michaelkovacic@Michaels-MacBook-Pro.local>
2018-02-01 12:34:38 -0700

    js updates

---
+++ assets/javascript/main.js
@@ -0,0 +1,13 @@
+
+
+$("#submit").on("click", function(){
+
+	var employeeName = $("#employee-name").val().trim();
+	var role = $("#role").val().trim();
+	var startDate = $("#start-date").val().trim();
+	var monthlyRate = $("#monthly-rate").val().trim();
+
+
+	
+
+})
