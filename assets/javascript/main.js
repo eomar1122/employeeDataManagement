3a8211af6165b12bfa610305127feb52cbf9ea12
Michael Kovacic <michaelkovacic@Michaels-MacBook-Pro.local>
2018-02-01 12:47:47 -0700

    link to firebase working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,42 @@
 
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCznoxkdOZFy0YB9YgJgMHVo1NxZbe8FvU",
+    authDomain: "datemanagement.firebaseapp.com",
+    databaseURL: "https://datemanagement.firebaseio.com",
+    projectId: "datemanagement",
+    storageBucket: "",
+    messagingSenderId: "159952801859"
+  };
+  firebase.initializeApp(config);
+
+
+
+var employeeName = "";
+var role = "";
+var startDate = "";
+var monthlyRate = "";
+
+var database = firebase.database();
+
+
 
 $("#submit").on("click", function(){
 
-	var employeeName = $("#employee-name").val().trim();
-	var role = $("#role").val().trim();
-	var startDate = $("#start-date").val().trim();
-	var monthlyRate = $("#monthly-rate").val().trim();
+	employeeName = $("#employee-name").val().trim();
+	role = $("#role").val().trim();
+	startDate = $("#start-date").val().trim();
+	monthlyRate = $("#monthly-rate").val().trim();
+
+	database.ref().push({
+		employeeName: employeeName,
+		role: role, 
+		startDate: startDate,
+		monthlyRate: monthlyRate
+	})
+
+	console.log(employeeName)
 
 
-	
 
 })
